4c9d4fd864951efca87f94d64ff0e42fcf4b2a15
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-09-03 09:03:22 +0100

    [Dashboard] Modify weather dashboard
    Problem
     * Weather temperature should be able to convert from celsius to farenheit
     * User should be able to toggle between these two states stated above
    
    Solution
     * Implemented logic to claculate and convert from one unit of measurement to another
     * Added onClick to the temperature element to toggle from celsius to farenheit and back
    
    [Finishes]

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
 // import api file here
 
 
-// Add this to an external filel and import it; Use standard convention for your folder structure mr.Mattieu!
+// Add this to an external file and import it; Use standard convention for your folder structure mr.Mattieu!
 
 
 window.addEventListener('load', () => {
-    // let long;
-    // let lat;
 
     let tempDescription = document.querySelector('.temperature-description');
     let tempDegree = document.querySelector('.temperature-degree');
     let locationTimeZone = document.querySelector('.location-timezone');
     let weatherIcon = document.querySelector('.weather-icon');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-          
-            let { longitude, latitude } = position.coords;
-            // long = position.coords.longitude;
-            // lat = position.coords.latitude; 
 
+            let { longitude, latitude } = position.coords;
+
+            // construct openWeatherApi url with long & lat from geolocation
             const openWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=1c831533900bcb8bf3edd9b4d16cfc69&units=metric`;
 
+            // make http request to OpenWeatherApi with fetch()
             fetch(openWeatherApiUrl)
-                 .then(response => {
-                     return response.json();
-                 })
-                 .then(weatherObject => {
-                     console.log('[OpenWeather Returns]: ', weatherObject);
-                     console.log('[Weather in Lagos]:', weatherObject.weather[0]);
-                     console.log('[CityName]:', weatherObject.name);
-                     
-                     // Destructure the properties in the weatherObject
-                     const { feels_like, humidity, pressure, temp, temp_max, temp_min} = weatherObject.main;
-                     const { description, icon, main } = weatherObject.weather[0];
-                     const {country, sunrise, sunset } = weatherObject.sys;
-                     
-                     // Set DOM Elements from weatherObject
-                     tempDegree.textContent = temp;
-                     tempDescription.textContent = firstCap(description);
-                     locationTimeZone.textContent = `${weatherObject.name}/${country}`;
-                     weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`;
-                 })
+                .then(response => {
+                    return response.json();
+                })
+                .then(weatherObject => {
+
+                    // Destructure the properties in the weatherObject
+                    const { feels_like, humidity, pressure, temp, temp_max, temp_min } = weatherObject.main;
+                    const { description, icon, main } = weatherObject.weather[0];
+                    const { country, sunrise, sunset } = weatherObject.sys;
+
+                    // Set DOM Elements from weatherObject
+                    tempDegree.textContent = temp;
+                    tempDescription.textContent = firstCap(description);
+                    locationTimeZone.textContent = `${weatherObject.name}/${country}`;
+                    weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`;
+
+                    // Convert from Celcius to Farenheit
+                    let celsius = (temp - 32) * (5 / 9);
+
+                    // change temperature to Celsius/Farenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.innerHTML === `<sup>o</sup>F`) {
+                            temperatureSpan.innerHTML = `<sup>o</sup>C`;
+                            tempDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.innerHTML = `<sup>o</sup>F`;
+                            tempDegree.textContent = temp;
+                        }
+                    })
+                })
         });
     }
 
@@ -49,6 +60,8 @@ window.addEventListener('load', () => {
 
 // Capitalize every first letter in a string 
 firstCap = (str) => {
- return str.toString().replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + 
- txt.substr(1).toLowerCase();});
+    return str.toString().replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() +
+            txt.substr(1).toLowerCase();
+    });
 }
